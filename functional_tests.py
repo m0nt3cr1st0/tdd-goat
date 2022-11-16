e997641d7b50a75579673ee9003a46074339d5a0
Gonzalo Rubio <gonzalo@bellmetric.net>
2022-11-16 09:36:29 +0100

    First FT specced out in comments, and refactor to use unittest. Add working log

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,43 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'install' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Julio has heard about a new cool online to-do app. He goes to check
+        # out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # He notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # He is invited to enter a to-do item straight away
+
+        # He types "Buy Magno shampoo" into a text box
+
+        # When he hits enter, the page updates, and now the page lists
+        # "1. Buy Magno shampoo" as an item in the to-do list
+
+        # There is still a text box inviting him to add another item
+        # He enters "Buy Frangelico"
+
+        # The page updates again, and now shows both items on his list
+
+        # Julio wonders if the site will remember his list. Then he see that the site
+        # has generated a unique URL for him -- there is some explanatory text
+        # to that effect.
+
+        # He visits that URL - his to-do list is still there
+
+        # Satisfied he goes back to sleep
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
---
+++ worklog.md
@@ -0,0 +1,7 @@
+# Day 1
+
+* Create dir and virtual environment
+* Install Django, Selenium and GeckoDrive
+* Write first Selenium functional test
+* Create Django project `django-admin startproject superlists .`
+* Start test server and see the test passing! `python manage.py runserver` 
